b778e7bd7d2625b52bd74be566dedf5b8c84fbb2
Bogdan <ciborg123@yandex.ru>
2025-04-22 16:05:50 +0300

    hot fix user id

--- app/api/v1/endpoints/chats.py
+++ app/api/v1/endpoints/chats.py
@@ -1,5 +1,8 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.core.security import get_current_user
+from app.db.models import User
 from app.db.session import get_db
 from app.db.crud.chat import get_chat, get_user_chats, create_chat
 
@@ -12,9 +15,9 @@ async def read_chat(chat_id: int, db: AsyncSession = Depends(get_db)):
         raise HTTPException(status_code=404, detail="Чат не найден")
     return chat
 
-@router.get("/chats/user/{user_id}")
-async def read_user_chats(user_id: int, db: AsyncSession = Depends(get_db)):
-    return await get_user_chats(db, user_id)
+@router.get("/chats/user/")
+async def read_user_chats(db: AsyncSession = Depends(get_db),current_user: User = Depends(get_current_user)):
+    return await get_user_chats(db, current_user.id)
 
 @router.post("/chats/")
 async def create_new_chat(user1_id: int, user2_id: int, db: AsyncSession = Depends(get_db)):
